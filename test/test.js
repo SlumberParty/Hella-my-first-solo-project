4c98e0a6a6fdb14960fface8626d4c1aa0d6b229
Claire Cassidy <clacassidy@gmail.com>
2019-04-26 11:47:27 -0700

    fixed test and added final styling

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import scoreResult from '../src/score-result';
+import scoreResult from '../index.js';
 
 const test = QUnit.test;
 
@@ -12,45 +12,44 @@ test('all answers are wrong', (assert) => {
     const questionFive = 'whyyy';
     const expected = 0;
 
+     //Act
+     // Call the function you're testing and set the result to a const
+    const result = scoreResult(questionOne, questionTwo, questionThree, questionFour, questionFive);
+    
+    //Assert
+    assert.equal(result, expected);
+ 
 });
 
-//     //Act
-//     // Call the function you're testing and set the result to a const
-//     const result = scoreResult(questionOne, questionTwo, questionThree, questionFour, questionFive);
+test('all answers right', (assert) => {
+     //Arrange
+    const questionOne = 'apples';
+    const questionTwo = 'mental health';
+    const questionThree = 'sloths';
+    const questionFour = 'no';
+    const questionFive = 'yes';
+    const expected = 5;
     
-//     //Assert
-//     assert.equal(result, expected);
-// });
+     //Act
+    const result = scoreResult(questionOne, questionTwo, questionThree, questionFour, questionFive);
+    
+     //Assert
+    assert.equal(result, expected);
+});
 
-//  test('all answers right', (assert) => {
-//     //Arrange
-//     const questionOne = 'apples';
-//     const questionTwo = 'mental health';
-//     const questionThree = 'sloths';
-//     const questionFour = 'no';
-//     const questionFive = 'yes';
-//     const expected = 5;
-    
-//     //Act
-//     const result = scoreResult(questionOne, questionTwo, questionThree, questionFour, questionFive);
-    
-//     //Assert
-//     assert.equal(result, expected);
-// });
+test('does lowercase work correctly', (assert) => {
+    //Arrange
+    const questionOne = 'NIght';
+    const questionTwo = 'rainbOWs';
+    const questionThree = 'oNe';
+    const questionFour = 'FRuit';
+    const questionFive = 'a lOt';
+    const expected = 5;
 
-// test('does lowercase work correctly', (assert) => {
-//     //Arrange
-//     const questionOne = 'NIght';
-//     const questionTwo = 'rainbOWs';
-//     const questionThree = 'oNe';
-//     const questionFour = 'FRuit';
-//     const questionFive = 'a lOt';
-//     const expected = 5;
-
-//     //Act
-//     const result = scoreResult(questionOne, questionTwo, questionThree, questionFour, questionFive);
+    //Act
+    const result = scoreResult(questionOne, questionTwo, questionThree, questionFour, questionFive);
     
-//     //Assert
-//     assert.equal(result, expected);
-// });
+    //Assert
+    assert.equal(result, expected);
+});
 
